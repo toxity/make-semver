6c11605f233e39e966079bb541b9dabfaa377ed9
im <im@norse.digital>
2017-03-22 12:33:53 +0200

    add different input types support

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
  * Created by im on 3/13/17.
  */
 module.exports = function (input, silence) {
+    input = String(input);
+
     var resultRegEx = /^((\d+\.)?(\d+\.)?(\*|\d+)((-[a-z]*)?(.\d+)?)?)$/,
         match = input.match(/(\d+\.)?(\d+\.)?(\d+)((-[a-z]+\.\d)|(-[a-z]+))?/),
         result = input, dots;
